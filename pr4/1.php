ff55e998650e06c3177c6185de000b72183eedc4
yaufdd <76522909+yaufdd@users.noreply.github.com>
2023-12-15 21:45:03 +0300

    Add files via upload

---
+++ pr4/1.php
@@ -0,0 +1,28 @@
+<?php
+
+if( isset( $_GET[ 'Submit' ] ) ) {
+    // Get input
+    $id = $_GET[ 'id' ];
+
+    // Check database
+    $getid  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
+    $result = mysqli_query($GLOBALS["_mysqli_ston"],  $getid ); // Removed 'or die' to suppress mysql errors
+
+    // Get results
+    $num = @mysqli_num_rows( $result ); // The '@' character suppresses errors
+    if( $num > 0 ) {
+        // Feedback for end user
+        echo '<pre>User ID exists in the database.</pre>';
+    }
+    else {
+        // User wasn't found, so the page wasn't!
+        header( $_SERVER[ 'SERVER_PROTOCOL' ] . ' 404 Not Found' );
+
+        // Feedback for end user
+        echo '<pre>User ID is MISSING from the database.</pre>';
+    }
+
+    ((isnull($mysqli_res = mysqli_close($GLOBALS["_mysqliston"]))) ? false : $mysqli_res);
+}
+
+?>
